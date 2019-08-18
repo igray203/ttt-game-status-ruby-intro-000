ce32490407abce735679321dd09850a025df7a9c
ian gray <igrayscale@gmail.com>
2019-08-18 15:22:45 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,16 +7,24 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 def won?(array)
-  WIN_COMBINATIONS.each do |winner|
-    if array(winner) == "X"
-      return winner
-    else
-      false
-    end
+#win_combination = [WIN_COMBINATION[0], WIN_COMBINATION[1], WIN_COMBINATION[2], WIN_COMBINATION[3], WIN_COMBINATION[4], WIN_COMBINATION[5], WIN_COMBINATION[6], WIN_COMBINATION[7]]
+#win_index_1 = win_combination[0][0]
+#win_index_2 = win_combination[0][1]
+#win_index_3 = win_combination[0][2]
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    position_1 = array[win_index_1]
+    position_2 = array[win_index_2]
+    position_3 = array[win_index_3]
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return win_combination
+      else
+        false
+      end
   end
 end
-
-
 #for each win_combination in WIN_COMBINATIONS
   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
   # grab each index from the win_combination that composes a win.
