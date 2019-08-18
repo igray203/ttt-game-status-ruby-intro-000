867ae1111c09c7abe0031c08990be21f70ef2d88
ian gray <igrayscale@gmail.com>
2019-08-18 15:31:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 def won?(array)
-  WIN_COMBINATIONS.find do |win_combination|
+  WIN_COMBINATIONS.each do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
@@ -16,6 +16,8 @@ def won?(array)
     position_3 = array[win_index_3]
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
         return win_combination
+      else
+        false
       end
   end
 end
