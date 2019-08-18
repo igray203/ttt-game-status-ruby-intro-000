fbc357f18ec91c178a8d172cd3ab97bdebcb13b9
ian gray <igrayscale@gmail.com>
2019-08-18 14:52:26 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,23 +4,24 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-array = ["X","X","X","","","","","",""]
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-def winning_rows(array)
-  if array == ["X", "X", "X"] || array == ["O", "O", "O"]
-    return win
-  else
-    return nil
+
+def won?(board)
+#  win_index_1 = WIN_COMBINATIONS[0]
+#  win_index_2 = WIN_COMBINATIONS[1]
+#  win_index_3 = WIN_COMBINATIONS[2]
+#  win_index_4 = WIN_COMBINATIONS[3]
+#  win_index_5 = WIN_COMBINATIONS[4]
+#  win_index_6 = WIN_COMBINATIONS[5]
+#  win_index_7 = WIN_COMBINATIONS[6]
+#  win_index_8 = WIN_COMBINATIONS[7]
+  WIN_COMBINATIONS.find do |winner|
+    winner == ["X","X","X"] || winner == ["O","O","O"]
   end
 end
 
-def won?
-  WIN_COMBINATIONS.detect do |combination|
-    winning_rows(combination).string?
-  end
-end
 
-#puts won?
+
 #for each win_combination in WIN_COMBINATIONS
   # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
   # grab each index from the win_combination that composes a win.
