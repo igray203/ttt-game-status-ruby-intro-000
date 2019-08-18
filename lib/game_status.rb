b5455058d2b490309f3ce8b037e2c174eba84a8b
ian gray <igrayscale@gmail.com>
2019-08-18 15:06:35 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,25 +7,10 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
 def won?(array)
-  win_index_1 = win_combination[0]
-  win_index_2 = win_combination[1]
-  win_index_3 = win_combination[2]
-  win_index_4 = win_combination[3]
-  win_index_5 = win_combination[4]
-  win_index_6 = win_combination[5]
-  win_index_7 = win_combination[6]
-  win_index_8 = win_combination[7]
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
-  position_4 = board[win_index_4]
-  position_5 = board[win_index_5]
-  position_6 = board[win_index_6]
-  position_7 = board[win_index_7]
-  position_8 = board[win_index_8]
+  win_combo["X","X","X"]
   WIN_COMBINATIONS.each do |winner|
-    if position_1 == "X" && position_2 == "X" && position_3 =="X"
-      return win_combination
+    if array(winner) == win_combo
+      return winner
     else
       false
     end
