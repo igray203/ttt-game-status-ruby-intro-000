9d480ab6a09507e36de1bd61aa9d43bf5cdbb930
ian gray <igrayscale@gmail.com>
2019-08-18 14:41:19 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,16 +6,16 @@ end
 # Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
-def winning_rows(array)
-  if array == ["X", "X", "X"] || array == ["O", "O", "O"]
-    return win
-  else
-    return nil
-  end
-end
 
 def won?
   WIN_COMBINATIONS.detect do |combination|
+    def winning_rows(array)
+      if array == ["X", "X", "X"] || array == ["O", "O", "O"]
+        return win
+      else
+        return nil
+      end
+    end
     winning_rows(combination).string?
   end
 end
