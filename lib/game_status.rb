9aef6241172fc523ed98128a2c78d852266c4c3c
ian gray <igrayscale@gmail.com>
2019-08-18 14:54:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-def won?
+def won?(array)
 #  win_index_1 = WIN_COMBINATIONS[0]
 #  win_index_2 = WIN_COMBINATIONS[1]
 #  win_index_3 = WIN_COMBINATIONS[2]
