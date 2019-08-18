04e845e509636b600b417f2303ad6a2fec5ca734
ian gray <igrayscale@gmail.com>
2019-08-18 14:54:29 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ def won?(array)
 #  win_index_7 = WIN_COMBINATIONS[6]
 #  win_index_8 = WIN_COMBINATIONS[7]
   WIN_COMBINATIONS.find do |winner|
-    winner == ["X","X","X"]
+    winner == ["X", "X", "X"]
   end
 end
 
